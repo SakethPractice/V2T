04baa1e2c783cb0cdcbdd0e62a33892655ed9d2b
Saketh <sakeththehyper@gmail.com>
2026-06-12 15:20:01 +0530

    format of question fields

---
+++ types/questions.ts
@@ -0,0 +1,23 @@
+export type QuestionType =
+  | "text"
+  | "number"
+  | "select"
+  | "date"
+  | "image";
+
+export type QuestionSection =
+  | "farmer"
+  | "farm"
+  | "block"
+  | "review";
+
+export interface Question {
+  id: string;
+  section: QuestionSection;
+  field: string;
+  question: string;
+  type: QuestionType;
+  required: boolean;
+
+  options?: string[];
+}
