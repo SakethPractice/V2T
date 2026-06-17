e84a140d57784038d405842c2d41600ecc2a1674
Saketh <sakeththehyper@gmail.com>
2026-06-17 16:20:48 +0530

    api added

--- backend/src/routes/sessionRoutes.js
+++ backend/src/routes/sessionRoutes.js
@@ -1,8 +1,11 @@
 import express from "express";
-import { startSession } from "../controllers/sessionController.js";
+import { startSession,completeSession,saveAnswer,saveSession } from "../controllers/sessionController.js";
 
 const router = express.Router();
 
 router.post("/start", startSession);
+router.post("/complete", completeSession);
+router.post("/save-answer", saveAnswer);
+router.post("/save",saveSession);
 
 export default router;
